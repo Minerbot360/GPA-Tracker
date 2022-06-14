4b9a54b357ef1467e895e2ffc5dd213a35910ee8
Liam Doran <65523663+Minerbot360@users.noreply.github.com>
2022-06-13 22:40:55 -0700

    I just made files
    Theres nothing :D I'm tired.

---
+++ GPA Tracker/Shared/ClassSelector.swift
@@ -0,0 +1,22 @@
+//
+//  ClassSelector.swift
+//  GPA Tracker
+//
+//  Created by Liam Doran on 6/13/22.
+//
+
+import SwiftUI
+
+struct ClassSelector: View {
+    var body: some View {
+        VStack() {
+            button("Class")
+        }
+    }
+}
+
+struct ClassSelector_Previews: PreviewProvider {
+    static var previews: some View {
+        ClassSelector()
+    }
+}
---
+++ GPA Tracker/Shared/GPA_TrackerApp.swift
@@ -0,0 +1,17 @@
+//
+//  GPA_TrackerApp.swift
+//  Shared
+//
+//  Created by Liam Doran on 6/13/22.
+//
+
+import SwiftUI
+
+@main
+struct GPA_TrackerApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
